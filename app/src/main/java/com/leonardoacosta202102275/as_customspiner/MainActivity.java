0814f0cdaf239a149444629993380b227c3bd541
LeonardoDacosta29 <dacostaleonardo137@gmail.com>
2023-05-04 20:25:12 +0700

    selesai

--- app/src/main/java/com/leonardoacosta202102275/as_customspiner/MainActivity.java
+++ app/src/main/java/com/leonardoacosta202102275/as_customspiner/MainActivity.java
@@ -3,12 +3,29 @@ package com.leonardoacosta202102275.as_customspiner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner1 = (Spinner) findViewById(R.id.spinner1);
+
+        List<String> negaralist = new ArrayList<>();
+        negaralist.add("Albania");
+        negaralist.add("Belgia");
+        negaralist.add("Hungary");
+        negaralist.add("Iran");
+        negaralist.add("Slovenia");
+
+        NegaraAdapter na = new NegaraAdapter(getApplicationContext(), negaralist);
+        spinner1.setAdapter(na);
     }
 }
--- app/src/main/java/com/leonardoacosta202102275/as_customspiner/NegaraAdapter.java
+++ app/src/main/java/com/leonardoacosta202102275/as_customspiner/NegaraAdapter.java
@@ -11,20 +11,20 @@ import android.widget.TextView;
 import java.util.List;
 
 public class NegaraAdapter extends BaseAdapter {
-    private Context context;
-    private List<String> negaraList;
+    private Context _context;
+    private List<String> _negaraList;
 
     public NegaraAdapter(Context _context, List<String> _negaraList) {
-        this.context = _context;
-        this.negaraList = _negaraList;
+        this._context = _context;
+        this._negaraList = _negaraList;
     }
 
     @Override
     public int getCount() {
         //return (negaraList != null) ? negaraList.size() : 0;
 
-        if (negaraList != null) {
-            return negaraList.size();
+        if (_negaraList != null) {
+            return _negaraList.size();
         }
         else {
             return 0;
@@ -43,19 +43,34 @@ public class NegaraAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        LayoutInflater inflater = LayoutInflater.from(context);
+        LayoutInflater inflater = LayoutInflater.from(_context);
         view = inflater.inflate(R.layout.spinner_a, null);
 
         TextView textView1 = (TextView) view.findViewById(R.id.textView1);
-        String negara = negaraList.get(i);
+        String negara = _negaraList.get(i);
         textView1.setText(negara);
 
         ImageView imageView1 = (ImageView) view.findViewById(R.id.imageView1);
-        
-        return null;
+
+        switch (negara){
+            case "Albania":
+                imageView1.setImageResource(R.drawable.albania);
+                break;
+            case "Belgia":
+                imageView1.setImageResource(R.drawable.belgia);
+                break;
+            case "Hungary":
+                imageView1.setImageResource(R.drawable.hungary);
+                break;
+            case "Iran":
+                imageView1.setImageResource(R.drawable.iran);
+                break;
+            case "Slovenia":
+                imageView1.setImageResource(R.drawable.slovenia);
+                break;
+
+        }
+
+        return view;
     }
-
-    @Override
-    public
-
 }
